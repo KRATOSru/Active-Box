6fd6cb357d5ccd46ffd33cf42f0121084f2c87be
VLADIMIR KARTYSHOV <ikartyshov50@gmail.com>
2022-07-31 03:10:15 +0300

    smooth scroll and nav toggle

--- app.js
+++ app.js
@@ -1,18 +1,49 @@
+// Fixed Header
 $(function () {
   let header = $("#header");
   let intro = $("#intro");
   let introH = intro.innerHeight();
   let scrollPos = $(window).scrollTop();
 
-  $(window).on("scroll load resize", function () {
-    introH = intro.innerHeight();
+  checkScroll(scrollPos, introH);
 
+  $(window).on("scroll resize", function () {
+    introH = intro.innerHeight();
     scrollPos = $(this).scrollTop();
 
+    checkScroll(scrollPos, introH);
+  });
+
+  function checkScroll(scrollPos, introH) {
     if (scrollPos > introH) {
       header.addClass("fixed");
     } else {
       header.removeClass("fixed");
     }
+  }
+
+  /* Smoth scrooll*/
+  $("[data-scroll]").on("click", function (event) {
+    event.preventDefault();
+
+    let elementId = $(this).data("scroll");
+    let elementOffset = $(elementId).offset().top;
+
+    $("html, body").animate(
+      {
+        scrollTop: elementOffset - 70,
+      },
+      700
+    );
+  });
+
+  /* Nav Toggle */
+  let nav = $("#nav");
+  let navToggle = $("#navToggle");
+
+  navToggle.on("click", function (event) {
+    event.preventDefault();
+
+    nav.toggleClass("show");
   });
 });
